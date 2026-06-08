6b6b0e93a14ab2523ffaec0e908398a4e844d3da
Sivasakthi <sakthisiva21022001@gmail.com>
2026-06-08 16:36:46 +0530

    backend url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://llm-chatbot-6jw4.vercel.app",
         changeOrigin: true,
       },
     },
